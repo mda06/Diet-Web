19a7847152e056311e6130d10645e534ada97c6d
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2017-12-20 18:03:16 +0100

    Login redirect to page when no access

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Login } from './login';
 import { LoginService} from './login.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,16 +14,21 @@ export class LoginComponent implements OnInit {
     username: 'caline@patient.com',
     password: 'caline'
   };
+  return: string = '';
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    //Check the return param and go to /login if nothing is set
+    this.route.queryParams
+      .subscribe(params => this.return = params['return'] || '/login');
   }
 
   onLogin() {
-    this.loginService.onLogin(this.login)
-      .subscribe((data) => {console.log("Data fetched: " + data.access_token);},
-            err => console.log("Cannot connect"));
+    this.loginService.onLogin(this.login);
+    this.router.navigateByUrl(this.return);
   }
 
 }
