fd073aed77b2372da3543ee53290669ccc53cb9a
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2017-12-25 20:56:28 +0100

    WIP: DatePicker with model

--- src/app/diet/detail-patient/detail-patient.component.ts
+++ src/app/diet/detail-patient/detail-patient.component.ts
@@ -1,6 +1,9 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 import {Patient} from "../../model/patient";
 import {DetailReturn} from "./detail-return";
+
+const now = new Date();
 
 @Component({
   selector: 'app-detail-patient',
@@ -12,6 +15,8 @@ export class DetailPatientComponent implements OnInit {
   @Input() patient: Patient;
   @Output() detailReturn = new EventEmitter<DetailReturn>();
   public isAddressCollapsed = true;
+  model: NgbDateStruct;
+  date: {year: number, month: number};
 
   constructor(/*private route: ActivatedRoute,
               private dietService: DietService*/) { }
@@ -23,6 +28,10 @@ export class DetailPatientComponent implements OnInit {
         data => this.patient = data
       );
     }*/
+  }
+
+  selectToday() {
+    this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
   }
 
   goBack() {
