92aadce878faff0920abb04faf880f15f5c14819
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-01-29 14:07:41 +0100

    WIP Menu design

--- src/app/diet/menu/main/main.component.ts
+++ src/app/diet/menu/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MenuService} from "../service/menu.service";
 import {Menu} from "../../../model/menu";
 import {Product} from "../../../model/product";
@@ -10,7 +10,9 @@ import {Product} from "../../../model/product";
 })
 export class MainComponent implements OnInit {
 
+  @ViewChild('accordion') accordion;
   selectedMenu: Menu;
+  showProducts; boolean = false;
 
   constructor(private service: MenuService) { }
 
@@ -19,10 +21,23 @@ export class MainComponent implements OnInit {
   }
 
   onDelete(tmpID: number) {
+    //Work's because: static-1 opens -> static-2 closes;
+    //After the toggle: static-2 opens because of the click
+    //But this won't work if we are static-1
+    this.accordion.toggle("static-1");
+    console.log(tmpID);
+  }
+
+  onEdit(tmpID: number) {
     console.log(tmpID);
   }
 
   onSelectedProduct(product: Product) {
     console.log("Product selected: ", product.id);
+  }
+
+  panelChange(evt: any) {
+    console.log(evt);
+    this.showProducts = evt.nextState;
   }
 }
