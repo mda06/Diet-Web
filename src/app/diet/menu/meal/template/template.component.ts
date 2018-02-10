9272e2bad7ea3dd1bdece3c6dd1969cb7fd35e85
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-02-10 17:02:40 +0100

    Added mealproducts in template and removed feedback of patient

--- src/app/diet/menu/meal/template/template.component.ts
+++ src/app/diet/menu/meal/template/template.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Meal} from '../../../../model/meal';
 import {MenuService} from '../../service/menu.service';
 import {AuthenticationService} from '../../../../services/authentication.service';
+import {TranslateService} from "@ngx-translate/core";
+import {FoodService} from "../../../food/service/food.service";
 
 @Component({
   selector: 'app-template',
@@ -13,12 +15,21 @@ export class TemplateComponent implements OnInit {
   templateMeals: Array<Meal> = [];
 
   constructor(private service: MenuService,
-              private authService: AuthenticationService) { }
+              private authService: AuthenticationService,
+              private foodService: FoodService,
+              public translate: TranslateService) { }
 
   ngOnInit() {
     this.authService.id.subscribe(id => {
       this.service.getMealByDietId(id).subscribe(data => {
         this.templateMeals = data;
+        this.templateMeals.forEach(meal => {
+          meal.mealProducts.forEach(mp => {
+            this.foodService.getProduct(mp.productId, this.translate.currentLang).subscribe(data => {
+              mp.product = data;
+            });
+          });
+        });
       }, err => console.log(err));
     });
   }
