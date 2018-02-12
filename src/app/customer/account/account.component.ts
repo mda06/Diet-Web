448c53a0c123e6a8a3074883d4a9ffef3e9c427b
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-02-12 23:52:34 +0100

    Update AccountComponent to works with a customer instead of only his id

--- src/app/customer/account/account.component.ts
+++ src/app/customer/account/account.component.ts
@@ -1,14 +1,15 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SignupAsk} from "../../model/signupAsk";
+import {Customer} from "../../model/customer";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
+export class AccountComponent<T extends Customer> implements OnInit {
 
-  @Input() customerId: number = 0;
+  @Input() customer: T;
   signup: SignupAsk = {customerId:0, email:"anthony-uniqueID@patient.com", password:""};
 
   constructor() { }
@@ -18,7 +19,14 @@ export class AccountComponent implements OnInit {
   }
 
   onCreateAccount() {
-    this.signup.customerId = this.customerId;
+    this.signup.customerId = this.customer.id;
+    console.log("Create");
+    console.log(this.signup);
+  }
+
+  onUpdateAccount() {
+    this.signup.customerId = this.customer.id;
+    console.log("Update");
     console.log(this.signup);
   }
 
