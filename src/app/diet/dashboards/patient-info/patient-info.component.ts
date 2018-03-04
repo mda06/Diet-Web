3663ab4ced0b07c11f90b70c51d0db45bed23f0d
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2018-03-04 11:53:33 +0100

    Moved patient info from dashboard to his item

--- src/app/diet/dashboards/patient-info/patient-info.component.ts
+++ src/app/diet/dashboards/patient-info/patient-info.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {DashboardItemComponent} from '../../../dashboard/dashboard-item/dashboard-item.component';
-import {IDashboardItem} from '../../../model/dashboardItem';
+import {SharedService} from "../../service/shared.service";
+import {Subscription} from "rxjs/Subscription";
+import {Patient} from "../../../model/patient";
+import {isNullOrUndefined} from "util";
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-patient-info',
@@ -9,9 +12,59 @@ import {IDashboardItem} from '../../../model/dashboardItem';
 })
 export class PatientInfoComponent implements OnInit {
 
-  constructor() { }
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ["Length", "Weight"];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+
+  public barChartData:any[] = [
+    {data: [180, 95], label: '12 Nov 2017'},
+    {data: [181, 90], label: '12 Dec 2017'}
+  ];
+
+  selectedPatient: Patient;
+  private subscriptions = new Subscription();
+
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
+    this.subscriptions.add(this.sharedService.patient$.subscribe(
+      data => {
+        if(!isNullOrUndefined(data)) {
+          this.selectedPatient = data;
+          /*let clone = _.clone(this.barChartData);
+          console.log(this.barChartData);
+          console.log(clone);
+          let values = [];*/
+          //this.barChartData.splice(0, this.barChartData.length);
+          //this.barChartLabels.splice(0, this.barChartLabels.length);
+          for(var param of this.selectedPatient.anthropometricParameters) {
+            //values.push({data:[param.length, param.weight],
+            //label:moment(param.date).format("DD/MM/YYYY")});
+          }
+
+          /*let clone = JSON.parse(JSON.stringify(this.barChartData));
+          for(var row of clone) {
+
+          }
+          clone[0].data = values[0].data;
+          clone[0].label = values[0].label;
+          this.barChartData = clone;*/
+        }
+      })
+    );
+  }
+
+  onPatientDeselect() {
+    this.sharedService.setPatient(null);
+    this.selectedPatient = null;
+  }
+
+  getAge(): number {
+    return moment().diff(this.selectedPatient.birthday, 'years',false);
   }
 
 }
--- src/app/diet/diet-dashboard/diet-dashboard.component.ts
+++ src/app/diet/diet-dashboard/diet-dashboard.component.ts
@@ -1,12 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Dietetist} from "../../model/dietetist";
-import {Patient} from "../../model/patient";
 import {SharedService} from "../service/shared.service";
 import {Subscription} from "rxjs/Subscription";
-import * as moment from 'moment';
-import {isNullOrUndefined} from "util";
 import {TranslateService} from "@ngx-translate/core";
-import {Router} from "@angular/router";
 import {IDashboardItem} from '../../model/dashboardItem';
 
 @Component({
@@ -22,24 +18,9 @@ export class DietDashboardComponent implements OnInit, OnDestroy {
   items = [this.foodItem, this.patientItem, this.foodSlider];
 
   diet: Dietetist;
-  selectedPatient: Patient;
   private subscriptions = new Subscription();
 
-  public barChartOptions:any = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
-  public barChartLabels:string[] = ["Length", "Weight"];
-  public barChartType:string = 'bar';
-  public barChartLegend:boolean = true;
-
-  public barChartData:any[] = [
-    {data: [180, 95], label: '12 Nov 2017'},
-    {data: [181, 90], label: '12 Dec 2017'}
-  ];
-
   constructor(public translate: TranslateService,
-              private router: Router,
               private sharedService: SharedService) { }
 
   ngOnInit() {
@@ -48,43 +29,9 @@ export class DietDashboardComponent implements OnInit, OnDestroy {
         this.diet = data;
       })
     );
-    this.subscriptions.add(this.sharedService.patient$.subscribe(
-      data => {
-        if(!isNullOrUndefined(data)) {
-          this.selectedPatient = data;
-          /*let clone = _.clone(this.barChartData);
-          console.log(this.barChartData);
-          console.log(clone);
-          let values = [];*/
-          //this.barChartData.splice(0, this.barChartData.length);
-          //this.barChartLabels.splice(0, this.barChartLabels.length);
-          for(var param of this.selectedPatient.anthropometricParameters) {
-            //values.push({data:[param.length, param.weight],
-              //label:moment(param.date).format("DD/MM/YYYY")});
-          }
-
-          /*let clone = JSON.parse(JSON.stringify(this.barChartData));
-          for(var row of clone) {
-
-          }
-          clone[0].data = values[0].data;
-          clone[0].label = values[0].label;
-          this.barChartData = clone;*/
-        }
-      })
-    );
-  }
-
-  onPatientDeselect() {
-    this.sharedService.setPatient(null);
-    this.selectedPatient = null;
   }
 
   ngOnDestroy() {
     this.subscriptions.unsubscribe();
-  }
-
-  getAge(): number {
-    return moment().diff(this.selectedPatient.birthday, 'years',false);
   }
 }
