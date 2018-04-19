5ce797571a0115490fdb1cc407978f1812bd997c
MDA06 <michaelvm171@msn.com>
2018-04-19 11:06:58 +0200

    Added a date selector for the diet when looking the meal pictures

--- src/app/diet/menu/meal-picture/meal-picture.component.ts
+++ src/app/diet/menu/meal-picture/meal-picture.component.ts
@@ -5,7 +5,10 @@ import {SharedService} from '../../service/shared.service';
 import {Subscription} from 'rxjs/Subscription';
 import {Patient} from '../../../model/patient';
 import {isNullOrUndefined} from "util";
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import * as moment from 'moment';
 
+const now = new Date();
 @Component({
   selector: 'app-meal-picture',
   templateUrl: './meal-picture.component.html',
@@ -14,7 +17,9 @@ import {isNullOrUndefined} from "util";
 export class MealPictureComponent implements OnInit, OnDestroy {
 
   mealPictures: Array<MealPicture> = [];
+  selectedMealPictures: Array<MealPicture> = [];
   selectedPatient: Patient;
+  selectedDate: NgbDateStruct = {day: now.getUTCDate(), month: now.getUTCMonth() + 1, year: now.getUTCFullYear()};
 
   private subscriptions = new Subscription();
 
@@ -31,19 +36,20 @@ export class MealPictureComponent implements OnInit, OnDestroy {
       })
     );
   }
+
   initModelFiles() {
     this.uploadService.getModelFiles(this.selectedPatient.id).subscribe(data => {
       this.mealPictures = data;
-      this.initMealPictures();
+      this.onDateChanged();
     },err => console.log(err))
   }
 
-  initMealPictures() {
-    this.mealPictures.forEach(pic => {
+  loadMealPictures() {
+    this.selectedMealPictures.forEach(pic => {
       if(pic.blob == null) {
         this.uploadService.getPicture(pic.id).subscribe(data => {
           const reader = new FileReader();
-          const mealPics = this.mealPictures;
+          const mealPics = this.selectedMealPictures;
           reader.addEventListener("load", function() {
             pic.blob = reader.result;
           });
@@ -52,6 +58,33 @@ export class MealPictureComponent implements OnInit, OnDestroy {
         }, err => console.log(err));
       }
     });
+  }
+
+  onDateChanged() {
+    const nativeDate = new Date();
+    nativeDate.setUTCFullYear(this.selectedDate.year, this.selectedDate.month - 1, this.selectedDate.day);
+    this.selectedMealPictures = this.findMealPicturesOf(nativeDate);
+    this.loadMealPictures();
+  }
+
+  findMealPicturesOf(date: Date): Array<MealPicture> {
+    console.log(date);
+    return this.mealPictures.filter(mp =>
+      mp.date.getUTCMonth() == date.getUTCMonth()
+      && mp.date.getUTCDate() == date.getUTCDate()
+      && mp.date.getUTCFullYear() == date.getUTCFullYear());
+  }
+
+  containsMealPicture(date: NgbDateStruct) {
+    const nativeDate = new Date();
+    nativeDate.setUTCFullYear(date.year, date.month - 1, date.day);
+   return this.findMealPicturesOf(nativeDate).length > 0;
+  }
+
+  getFormattedDate(date: NgbDateStruct): string {
+    const nativeDate = new Date();
+    nativeDate.setUTCFullYear(date.year, date.month - 1, date.day);
+    return moment(nativeDate).format("DD/MM/YYYY");
   }
 
   ngOnDestroy() {
